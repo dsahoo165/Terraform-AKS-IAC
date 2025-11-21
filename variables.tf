d3d2be741ccf4e48f3c06e207e5b94ed889d4247
Deepak <dsahoo165@gmail.com>
2025-11-21 20:04:04 +0530

    empty repo

---
+++ variables.tf
@@ -0,0 +1,37 @@
+# Azure Authentication Variables
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_client_id" {
+  description = "Azure Service Principal Client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_client_secret" {
+  description = "Azure Service Principal Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-aks-lab"
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "centralindia"
+}
+
